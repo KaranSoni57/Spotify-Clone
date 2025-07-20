a05a965c01d6ac143d7199fbfbea903a23b05336
KaranSoni57 <karansoni337d@gmail.com>
2025-07-20 14:09:58 +0530

    little close to the end

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("Let's Go!");
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
+}
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  console.log(songUL);
+  for (const song of songs) {
+    songUL.innerHTML += `<li>
+    
+              <img
+                class="invert"
+                src="Images/music-note-svgrepo-com.svg"
+                alt=""
+              />
+              <div class="songinfo">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>Artist</div>
+              </div> 
+              <img class="invert" src="Images/play-svgrepo-com.svg" alt="" />
+            
+     </li>`;
+  }
+
+  var audio = await new Audio(songs[0]);
+  audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(duration);
+  });
+}
+main();
